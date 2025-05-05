f74908d479159fe92235c7f9bc1117ace9097267
Nikolays878 <kola69429@gmail.com>
2025-05-05 22:53:28 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.filters import Command, CommandStart
 import logging
 
-API_TOKEN = "7612817210:AAE9b2Q4mnljmWs10L0boPhpYHT0qYB8ta0"
+API_TOKEN = "7612817210:AAG8x_0a2fCVRtt6njrwKme4I_EuxNB0pdg"
 GROUP_CHAT_ID = -1002368509151  # Заменить на ID вашей группы
 
 logging.basicConfig(level=logging.INFO)
